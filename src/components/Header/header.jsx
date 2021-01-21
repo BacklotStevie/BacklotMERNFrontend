c20f9ef0c94000ff83ef7592960daa5d6522858c
Steven Sarria <ssarr012@fiu.edu>
2021-01-21 16:28:36 -0500

    added root font and styling

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -22,17 +22,17 @@ const Header = () => {
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" className="mr-5 ml-5">
                 <Navbar.Brand href="#home">
                     <Link to="/home">
-                        <img className="logo" src={Logo} alt='logo' width="50" height="50" />
+                        <img className="header-logo" src={Logo} alt='logo' width="50" height="50" />
                     </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto text-right">
-                        <Link to="/reviews"><Nav.Link href="#reviews">Film Reviews</Nav.Link></Link>
-                        <Link to="/podcastpage"><Nav.Link href="#podcasts">Podcast</Nav.Link></Link>
-                        <Link to="/videospage"><Nav.Link href="#videos">Videos</Nav.Link></Link>
-                        <Link to="/teampage"><Nav.Link href="#team">Team</Nav.Link></Link>
-                        <Link to="/aboutus"><Nav.Link href="#aboutus">About Us</Nav.Link></Link>
+                        <Link to="/reviews"><Nav.Link className="nav-buttons mx-2" href="#reviews">Film Reviews</Nav.Link></Link>
+                        <Link to="/podcastpage"><Nav.Link className="nav-buttons mx-2" href="#podcasts">Podcast</Nav.Link></Link>
+                        <Link to="/videospage"><Nav.Link className="nav-buttons mx-2" href="#videos">Videos</Nav.Link></Link>
+                        <Link to="/teampage"><Nav.Link className="nav-buttons mx-2" href="#team">Team</Nav.Link></Link>
+                        <Link to="/aboutus"><Nav.Link className="nav-buttons mx-2" href="#aboutus">About Us</Nav.Link></Link>
                         {user ? <Link onClick={logout}><Nav.Link href="#signup">Log Out</Nav.Link></Link> : <Link to="/signup"><Nav.Link href="#signup">Sign Up</Nav.Link></Link>}
                     </Nav>
                 </Navbar.Collapse>

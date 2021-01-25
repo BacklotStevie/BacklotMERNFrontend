caf6c522ce196f0218265164fb79bb3462e91112
Steven Sarria <ssarr012@fiu.edu>
2021-01-25 15:02:13 -0500

    changed podcasts and jumbo entries

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -4,7 +4,7 @@ import { CardDeck } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 import Logo from '../../Images/backlot-logo.png'
 import Disney from '../../Images/Disney Investory Day.jpg'
-import Mando from '../../Images/mando.jpg'
+import Wolfwalkers from '../../Images/wolfwalkers.jpg'
 import './homepage.css'
 
 const HomePage = () => {
@@ -13,11 +13,11 @@ const HomePage = () => {
             <div className="row">
                 <div className="jumbotron col-12 home-jumbo pb-2 pt-5 mt-5 pr-5 d-flex align-items-center bg-medium">
                     <div className="col-8 mb-5">
-                        <img className="featured-jumbo" src="https://cf-images.us-east-1.prod.boltdns.net/v1/static/6210937530001/a5814f4b-5bea-4879-a34b-b57989377adb/00b3c7bc-40f0-48ba-94de-3516dc250297/1280x720/match/image.jpg" />
+                        <img className="featured-jumbo" src="https://cnet2.cbsistatic.com/img/6hBpTES0Zkehs04k5kZkUQ7wdzM=/940x0/2021/01/13/12b59056-0831-4889-b7ff-1f0910bc1505/wandavision-disney-plus-marvel-ep16.jpg" />
                     </div>
                     <div className="col-4" >
-                        <h1 className="display-5 text-black mr-4 mb-4">Disney Investor Day Mayhem</h1>
-                        <p className="lead text-black mr-4 mb-5">John and Steven discuss the madness of Disney Investor Day.<br /><a href='https://podcasts.apple.com/us/podcast/disney-investor-day-mayhem/id1460214563?i=1000502128805'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/1DePsWGat43dXBHpJ6Lvlt?si=0fH73pHKS7GRMLWKVCEnDw'>Listen on Spotify</a></p>
+                        <h1 className="display-5 text-black mr-4 mb-4">WandaVision Episodes 1 & 2</h1>
+                        <p className="lead text-black mr-4 mb-5">Marvel Phase 4 is finally here! Steven and John discuss the much-anticipated first entry in the MCU since July 2019.<br /><a href='https://podcasts.apple.com/us/podcast/disney-investor-day-mayhem/id1460214563?i=1000502128805'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/1DePsWGat43dXBHpJ6Lvlt?si=0fH73pHKS7GRMLWKVCEnDw'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -30,23 +30,11 @@ const HomePage = () => {
 
             <CardDeck className="podcasts pb-5">
                 <Card className='podcast-card'>
-                    <Card.Img variant="top" src={Disney} />
+                    <Card.Img variant="top" src="https://cnet2.cbsistatic.com/img/6hBpTES0Zkehs04k5kZkUQ7wdzM=/940x0/2021/01/13/12b59056-0831-4889-b7ff-1f0910bc1505/wandavision-disney-plus-marvel-ep16.jpg" />
                     <Card.Body className="card-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>Disney Investor Day Madness</Card.Title>
+                        <Card.Title style={{ fontSize: "2rem" }}>WandaVision Episodes 1 & 2</Card.Title>
                         <Card.Text className="podcast-text">
-                            Steven and John convene to dicuss (mostly) everything announced at Disney Investor Day
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='//linktr.ee/insidethebacklot/'>Listen Now</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src={Mando} />
-                    <Card.Body className="card-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>HBO Max & <i>The Mandalorian</i></Card.Title>
-                        <Card.Text className="podcast-text">
-                            Steven and John discuss the latest HBO Max news, the last two episodes of <i>The Mandalorian</i> and Cameron Crowe's <i>Almost Famous.</i>
+                            Steven and John discuss the first two episodes of <i>WandaVision.</i>
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
@@ -54,11 +42,23 @@ const HomePage = () => {
                     </Card.Footer>
                 </Card>
                 <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://cdn.onebauer.media/one/media/5f3f/a5cc/4074/8e61/f349/288d/tenet-rev-main.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg" />
+                    <Card.Img variant="top" src={Wolfwalkers} />
                     <Card.Body className="card-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>Swolelock Holmes, Meth Damon and More!</Card.Title>
+                        <Card.Title style={{ fontSize: "2rem" }}>Mando S2 Finale, Wolfwalkers & Sound of Metal</Card.Title>
                         <Card.Text className="podcast-text">
-                            JP fills in for John as he and Steven discuss <i>Tenet</i> and Netflix's <i>Enola Holmes</i> & <i>The Devil All the Time.</i>
+                            Steven and John discuss <i>The Mandalorian's</i> season finale, as well as <i>Wolfwalkers</i> and <i>Sound of Metal.</i>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='//linktr.ee/insidethebacklot/'>Listen Now</a></h6>
+                    </Card.Footer>
+                </Card>
+                <Card className='podcast-card'>
+                    <Card.Img variant="top" src={Disney} />
+                    <Card.Body className="card-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>Disney Investor Day Madness</Card.Title>
+                        <Card.Text className="podcast-text">
+                            Steven and John convene to dicuss (mostly) everything announced at Disney Investor Day
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>

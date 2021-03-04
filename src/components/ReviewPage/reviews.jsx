bfaca4e0f8961bfab1ce9c823045c7a193399f94
Steven Sarria <stevensarria@Matthews-MBP.attlocal.net>
2021-03-04 15:19:40 -0500

    finished phase 1 of site

--- src/components/ReviewPage/reviews.jsx
+++ src/components/ReviewPage/reviews.jsx
@@ -16,36 +16,38 @@ const ReviewsList = (props) => {
             })
     }, [])
 
+    let width = ""
+
     const ShowReviews = () => {
         return reviews && reviews.map((review, i, arr) => {
             return (
-                <div className="story col-lg-3 col-sm-12 mb-5">
-                    <div className="story-info card w-100 h-100">
-                        <img className="card-img " src={review.img} alt="card" />
-                        <div className="card-img-overlay">
-                            <Link to={`/reviews/${review._id}`}><h1>{review.title}</h1></Link>
-                            <h4>{review.writer}</h4>
-                        </div>
+                // <div className="story col-lg-3 col-sm-12 mb-5">
+                //     <div className="story-info card w-100 h-100">
+                //         <img className="card-img " src={review.img} alt="card" />
+                //         <div className="card-img-overlay">
+                //             <Link to={`/reviews/${review._id}`}><h1>{review.title}</h1></Link>
+                //             <h4>{review.writer}</h4>
+                //         </div>
+                //     </div>
+                // </div>
+
+
+            <div className=" col-lg-3 mb-4">
+                <div className=" thumbnail text-center">
+                   <img src={review.img} alt="image card" className=" reviews-image"/>
+                   <div className=" caption">
+                        <Link to={`/reviews/${review._id}`}><h1>{review.title}</h1></Link>
+                        <h4>{review.writer}</h4>
                     </div>
-
-                    {/* <div>
-                            {review.reviews?.map((nestedReview) =>
-                                <>
-                                    <h3>{nestedReview.heading}</h3>
-                                    <p>{nestedReview.info}</p>
-                                </>
-                            )}
-                        </div> */}
                 </div>
-
-
+            </div>
             )
         })
     }
 
 
     return (
-        <div className="container-fluid p-4">
+        <div className="container">
             <div className="row">
                 <ShowReviews />
             </div>
--- src/pages/ReviewPage/ReviewPage.js
+++ src/pages/ReviewPage/ReviewPage.js
@@ -4,26 +4,43 @@ import ReviewsList from '../../components/ReviewPage/reviews';
 
 const ReviewPage = (props) => {
     return (
-        <div className="container-fluid">
-            <div className="row review-container">
-                <div className="container text-center">
-                    <button className="divider-button bg-dark" disabled></button>
-                    <h1 className="pt-3">Backlot Reviews</h1>
-                </div>
-                <div className="container w-10">
-                    <ReviewsList />
-                </div>
+        // <div className="container-fluid">
+        //     <div className="row review-container">
+        //         <div className="text-center">
+        //             <button className="divider-button bg-dark" disabled></button>
+        //             <h1 className="pt-3">Backlot Reviews</h1>
+        //         </div>
 
-                <div className="container text-center pt-5">
+        //         <div className="">
+        //             <ReviewsList />
+        //         </div>
+
+        //         <div className="container text-center pt-5">
+        //             <button className="divider-button bg-dark mt-4" disabled></button>
+        //             <h1 className="pt-3">Collections</h1>
+        //             <br />
+        //             <h3 className='mt-5'>Coming Soon</h3>
+        //         </div>
+
+        //     </div>
+        // </div>
+
+        <div className=" container">
+            <div className=" row">
+                <div className=" text-center col-12">
+                    <button className="divider-button bg-dark" disabled></button>
+                    <h1 className="p-3">Backlot Reviews</h1>
+                </div>
+                    <ReviewsList />
+                <div className=" text-center col-12">
                     <button className="divider-button bg-dark mt-4" disabled></button>
                     <h1 className="pt-3">Collections</h1>
-                    <br />
+                </div>
+                <div className=" text-center col-12">
                     <h3 className='mt-5'>Coming Soon</h3>
                 </div>
-
             </div>
         </div>
-
     );
 };
 

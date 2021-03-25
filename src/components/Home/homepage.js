8fb74d07af98d95b01dc69de095f615ac9271daf
Steven Sarria <ssarr012@fiu.edu>
2021-03-25 13:30:31 -0400

    updated homepage

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -13,11 +13,11 @@ const HomePage = () => {
             <div className="">
                 <div className="jumbotron home-jumbo mt-5 pt-5 d-flex flex-wrap bg-medium">
                     <div className="col-12 col-lg-6">
-                        <img className="featured-jumbo" src="https://www.denofgeek.com/wp-content/uploads/2021/03/wandavision-episode-9-paul-bettany-elizabeth-olsen.jpeg?fit=3840%2C2160" />
+                        <img className="featured-jumbo" src="https://cdn.mos.cms.futurecdn.net/QMpD8xrRXF7QoZwgX2XpPE-1200-80.jpg" />
                     </div>
                     <div className="col-12 col-lg-6" >
-                        <h1 className="display-5 pt-5 text-black">WandaVision Finale & Raya and the Last Dragon</h1>
-                        <p className="lead text-black">Steven and John discuss the season finale of <i>WandaVision</i> before discussing Disney's <i>Raya and the Last Dragon</i>.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563#episodeGuid=insidethebacklot.podbean.com%2F4f8160f8-6d36-3baf-aee0-ff48d80f5576'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/42cWg4mkDTz0yCQrGx9FRh?si=fvAcqvVGQImDZaU5biZJ6Q'>Listen on Spotify</a></p>
+                        <h1 className="display-5 pt-5 text-black">Zack Snyder's Justice League</h1>
+                        <p className="lead text-black">The boys discuss their thoughts and criticisms of the long-awaited Snydercut.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563#episodeGuid=insidethebacklot.podbean.com%2Ff5fb0705-7723-3bcf-a7ea-807ee3fc2627'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/3Xt9I51IH6cIew2b081Syu?si=hCfLZFSvRy65ozOePbGS2A'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -30,6 +30,19 @@ const HomePage = () => {
 
             <CardDeck className="podcasts pb-5">
                 <Card className='podcast-card'>
+                    <Card.Img variant="top" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/03/zack-snyders-justice-league-team-social.jpg" />
+                    <Card.Body className="podcast-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>Zack Snyder's Justice League</Card.Title>
+                        <Card.Text className="podcast-text">
+                            The boys discuss their thoughts and criticisms of the long-awaited Snydercut.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='https://open.spotify.com/episode/3Xt9I51IH6cIew2b081Syu?si=xU7sEKV4RJiHJ5U_fx4-4w'>Listen On Spotify</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563#episodeGuid=insidethebacklot.podbean.com%2Ff5fb0705-7723-3bcf-a7ea-807ee3fc2627'>Listen On Apple Podcasts</a></h6>
+                    </Card.Footer>
+                </Card>
+                <Card className='podcast-card'>
                     <Card.Img variant="top" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/12/raya-and-the-last-dragon-social-1.jpg" />
                     <Card.Body className="podcast-body">
                         <Card.Title style={{ fontSize: "2rem" }}>WandaVision Finale & Raya and the Last Dragon</Card.Title>
@@ -39,7 +52,7 @@ const HomePage = () => {
                     </Card.Body>
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/42cWg4mkDTz0yCQrGx9FRh?si=fvAcqvVGQImDZaU5biZJ6Q'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563#episodeGuid=insidethebacklot.podbean.com%2F4f8160f8-6d36-3baf-aee0-ff48d80f5576'>Listen On Apple Podcasts</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000512222954'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
                 <Card className='podcast-card'>
@@ -47,25 +60,12 @@ const HomePage = () => {
                     <Card.Body className="podcast-body">
                         <Card.Title style={{ fontSize: "2rem" }}>Golden Globes + WandaVision</Card.Title>
                         <Card.Text className="podcast-text">
-                        Steven and John discuss this weekend's Golden Globes and episodes 7 & 8 of <i>WandaVision</i>.
+                            Steven and John discuss this weekend's Golden Globes and episodes 7 & 8 of <i>WandaVision</i>.
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/3vOHndpARiIbbe3w9Azoao?si=GlR2-Es9S5ybrvHMMLmlAQ'>Listen On Spotify</a></h6>
                         <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000511378732'>Listen On Apple Podcasts</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://variety.com/wp-content/uploads/2020/11/Judas-and-the-Black-Messiah.jpg?w=681&h=383&crop=1g" />
-                    <Card.Body className="podcast-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>WandaVision, Judas and the Black Messiah & More</Card.Title>
-                        <Card.Text className="podcast-text">
-                            Steven and John discuss episodes 5 & 6 of <i>WandaVision</i>, as well as some other films they watched that week.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='https://open.spotify.com/episode/0xlT6igfbYvBTcoge1svRl?si=HJv7CnhZQBmKdn_UObwQXQ'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000509702676'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
             </CardDeck>

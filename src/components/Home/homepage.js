f78712d59b34012eef23ce3d5d0139465e6966b9
Steven Sarria <ssarr012@fiu.edu>
2021-04-28 16:27:42 -0400

    Updated homepage
    Updated site homepage to reflect newest podcast episode release.

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -13,11 +13,11 @@ const HomePage = () => {
             <div className="">
                 <div className="jumbotron home-jumbo mt-5 pt-5 d-flex flex-wrap bg-medium">
                     <div className="col-12 col-lg-6">
-                        <img className="featured-jumbo" src="https://www.denofgeek.com/wp-content/uploads/2021/03/Godzilla-vs-Kong-3.jpeg?resize=1024,592" />
+                        <img className="featured-jumbo" src="https://miro.medium.com/max/3840/0*CnpPQBdzDHEYcsD2.jpeg" />
                     </div>
                     <div className="col-12 col-lg-6" >
-                        <h1 className="display-5 pt-5 text-black">Godzilla vs Kong</h1>
-                        <p className="lead text-black">JP joins the team as the boys discuss Warner Bros' latest film, <i>Godzilla vs Kong.</i><br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000517133533'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/0D0s8c37HfljN5QlBmNcSf?si=Fw6gdHUXRpSEWmf4VaP1jw'>Listen on Spotify</a></p>
+                        <h1 className="display-5 pt-5 text-black">93rd Annual Academy Awards</h1>
+                        <p className="lead text-black">Steven and John discuss this year's Academy Awards ceremony.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000518874957'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/6ya8oqcI7suFTGCtmpugSc?si=94608300844c4f24'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -29,6 +29,19 @@ const HomePage = () => {
             <h3 className="text-center sections">PODCASTS</h3>
 
             <CardDeck className="podcasts pb-5">
+                <Card className='podcast-card'>
+                    <Card.Img variant="top" src="https://miro.medium.com/max/3840/0*CnpPQBdzDHEYcsD2.jpeg" />
+                    <Card.Body className="podcast-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>93rd Annual Academy Awards Recap</Card.Title>
+                        <Card.Text className="podcast-text">
+                            Steven and John discuss the odd choices made during this year's 'Oscars' ceremony. 
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='https://open.spotify.com/episode/6ya8oqcI7suFTGCtmpugSc?si=70d3e7e3f9d44299'>Listen On Spotify</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=10005188749573'>Listen On Apple Podcasts</a></h6>
+                    </Card.Footer>
+                </Card>
                 <Card className='podcast-card'>
                     <Card.Img variant="top" src="https://www.indiewire.com/wp-content/uploads/2021/01/Screen-Shot-2021-01-24-at-9.29.56-AM.png?w=780" />
                     <Card.Body className="podcast-body">
@@ -53,19 +66,6 @@ const HomePage = () => {
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/3Xt9I51IH6cIew2b081Syu?si=xU7sEKV4RJiHJ5U_fx4-4w'>Listen On Spotify</a></h6>
                         <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000514394594'>Listen On Apple Podcasts</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/12/raya-and-the-last-dragon-social-1.jpg" />
-                    <Card.Body className="podcast-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>WandaVision Finale & Raya and the Last Dragon</Card.Title>
-                        <Card.Text className="podcast-text">
-                        Steven and John discuss the season finale of <i>WandaVision</i> & <i>Raya and the Last Dragon.</i>
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='https://open.spotify.com/episode/42cWg4mkDTz0yCQrGx9FRh?si=fvAcqvVGQImDZaU5biZJ6Q'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000512222954'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
             </CardDeck>

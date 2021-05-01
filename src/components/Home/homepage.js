8f58b3863c6a4fbf13ebe4e3842eb5a362b09d71
Steven Sarria <ssarr012@fiu.edu>
2021-05-01 02:03:06 -0400

    Updated homepage
    Update homepage to reflect current content.

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -13,11 +13,11 @@ const HomePage = () => {
             <div className="">
                 <div className="jumbotron home-jumbo mt-5 pt-5 d-flex flex-wrap bg-medium">
                     <div className="col-12 col-lg-6">
-                        <img className="featured-jumbo" src="https://miro.medium.com/max/3840/0*CnpPQBdzDHEYcsD2.jpeg" />
+                        <img className="featured-jumbo" src="https://m.media-amazon.com/images/M/MV5BNjNjMTdkNjUtNzRmNi00ZTZjLTgzMDUtNzBhNTVmODUwM2QxXkEyXkFqcGdeQWFybm8@._V1_.jpg" />
                     </div>
                     <div className="col-12 col-lg-6" >
-                        <h1 className="display-5 pt-5 text-black">93rd Annual Academy Awards</h1>
-                        <p className="lead text-black">Steven and John discuss this year's Academy Awards ceremony.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000518874957'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/6ya8oqcI7suFTGCtmpugSc?si=94608300844c4f24'>Listen on Spotify</a></p>
+                        <h1 className="display-5 pt-5 text-black">Mortal Kombat</h1>
+                        <p className="lead text-black">Steven, JP and John pontificate about this year's indie gem, <i>Mortal Kombat</i>.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000519515819'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/5VvfYv5Bzm6BveQcMMz0cJ?si=a55e060313984fef'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -30,11 +30,24 @@ const HomePage = () => {
 
             <CardDeck className="podcasts pb-5">
                 <Card className='podcast-card'>
+                    <Card.Img variant="top" src="https://zambiainc.com/wp-content/uploads/2021/04/Mortal-Kombat.jpg" />
+                    <Card.Body className="podcast-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>Mortal Kombat</Card.Title>
+                        <Card.Text className="podcast-text">
+                            Steven, JP & John pontificate about this year's indie gem, <i>Mortal Kombat.</i> 
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='https://open.spotify.com/episode/5VvfYv5Bzm6BveQcMMz0cJ?si=a55e060313984fef'>Listen On Spotify</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000519515819'>Listen On Apple Podcasts</a></h6>
+                    </Card.Footer>
+                </Card>
+                <Card className='podcast-card'>
                     <Card.Img variant="top" src="https://miro.medium.com/max/3840/0*CnpPQBdzDHEYcsD2.jpeg" />
                     <Card.Body className="podcast-body">
                         <Card.Title style={{ fontSize: "2rem" }}>93rd Annual Academy Awards Recap</Card.Title>
                         <Card.Text className="podcast-text">
-                            Steven and John discuss the odd choices made during this year's 'Oscars' ceremony. 
+                            Steven and John discuss the odd choices made during this year's Oscars ceremony.
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
@@ -43,7 +56,7 @@ const HomePage = () => {
                     </Card.Footer>
                 </Card>
                 <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://www.indiewire.com/wp-content/uploads/2021/01/Screen-Shot-2021-01-24-at-9.29.56-AM.png?w=780" />
+                    <Card.Img variant="top" src="https://cdn.onebauer.media/one/media/605b/69a9/dbf2/7f24/48f5/9560/godzilla-vs-kong.jpg?format=jpg&quality=80&width=850&ratio=16-9&resize=aspectfill" />
                     <Card.Body className="podcast-body">
                         <Card.Title style={{ fontSize: "2rem" }}>Godzilla vs Kong</Card.Title>
                         <Card.Text className="podcast-text">
@@ -53,19 +66,6 @@ const HomePage = () => {
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/0D0s8c37HfljN5QlBmNcSf?si=Fw6gdHUXRpSEWmf4VaP1jw'>Listen On Spotify</a></h6>
                         <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000517133533'>Listen On Apple Podcasts</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/03/zack-snyders-justice-league-team-social.jpg" />
-                    <Card.Body className="podcast-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>Zack Snyder's Justice League</Card.Title>
-                        <Card.Text className="podcast-text">
-                            The boys discuss their thoughts and criticisms of the long-awaited Snydercut.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='https://open.spotify.com/episode/3Xt9I51IH6cIew2b081Syu?si=xU7sEKV4RJiHJ5U_fx4-4w'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000514394594'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
             </CardDeck>

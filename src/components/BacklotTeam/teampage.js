51c672e0575d0ac3a251d79888c3a4e0fd99e56e
Steven Sarria <ssarr012@fiu.edu>
2021-06-11 14:57:58 -0400

    Updating team page
    Began updating teampage to add new podcast co-host.

--- src/components/BacklotTeam/teampage.js
+++ src/components/BacklotTeam/teampage.js
@@ -34,6 +34,31 @@ const TeamPage = () => {
                         </Card.Footer> */}
                     </Card>
                 </div>
+                
+                <div className="col-md-6">
+                    <Card className='border-0'>
+                        <Card.Img width="100%" variant="top" src={JP} />
+                        <Card.Body className="card-body">
+                            <Card.Title className="text-center mb-2">
+                                <h3 class="mb-3">JP Matias</h3>
+                                <h6>
+                                    <i>Co-Host</i>
+                                </h6>
+                            </Card.Title>
+                            <Card.Text className="podcast-text">
+                                The founder of Inside the Backlot, the company started as a passion project of Steven's. An aspiring filmmaker and film lover, Steven took an interest in the art form at an early age, and the passion has grown since. With a penchant for not shutting up, Inside the Backlot serves as an outlet for him - and others to come together and share discussion in something they love. When not recording the podcast, you can catch Steven convincing his friends to watch <i>Black Sails</i> or re-watching <i>Sherlock</i> for the 16th time. 
+                            </Card.Text>
+                            <div className='text-center mt-3'>
+                                <a href="https://twitter.com/sarria_steven?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png" className="social-icons-team"></img></a>
+                                <a href="https://www.instagram.com/steven_sarriaa/"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" className="social-icons-insta"></img></a>
+                                <a href="https://www.youtube.com/channel/UCjzTSGj_1r7K-W3kzZGYIdw"><img src="https://i.pinimg.com/originals/de/1c/91/de1c91788be0d791135736995109272a.png" className="social-icons-team"></img></a>
+                            </div>
+                        </Card.Body>
+                        {/* <Card.Footer className='p-5 m-5'>
+                            <h6 className="text-center"><a href='//linktr.ee/insidethebacklot/'>Listen Now</a></h6>
+                        </Card.Footer> */}
+                    </Card>
+                </div>
 
                 <div className="col-md-6">
                     <Card className='border-0'>

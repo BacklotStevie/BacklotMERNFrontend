58fee0482af5897cac6ad91216d2641ea4558068
Steven Sarria <ssarr012@fiu.edu>
2021-07-02 15:08:20 -0400

    Update teampage
    Updated teampage to add info for new podcast host.

--- src/components/BacklotTeam/teampage.js
+++ src/components/BacklotTeam/teampage.js
@@ -46,11 +46,11 @@ const TeamPage = () => {
                                 </h6>
                             </Card.Title>
                             <Card.Text className="podcast-text">
-                                Producer, podcast host, 
+                                Producer, podcast host, #1 fan. JP joined the Backlot team earlier this year, after being a frequent guest on the show. Rounding out the Backlot trio, his knowledge of music and film caps off the team, although his ability to reel in his other two co-hosts is quite possibly the best thing for our listeners. 
                             </Card.Text>
                             <div className='text-center mt-3'>
                                 <a href="https://twitter.com/sarria_steven?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png" className="social-icons-team"></img></a>
-                                <a href="https://www.instagram.com/steven_sarriaa/"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" className="social-icons-insta"></img></a>
+                                <a href="https://www.instagram.com/_paulymath/?hl=en"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" className="social-icons-insta"></img></a>
                                 <a href="https://www.youtube.com/channel/UCjzTSGj_1r7K-W3kzZGYIdw"><img src="https://i.pinimg.com/originals/de/1c/91/de1c91788be0d791135736995109272a.png" className="social-icons-team"></img></a>
                             </div>
                         </Card.Body>

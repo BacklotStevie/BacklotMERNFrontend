94caa381a804896994c879fd2247ae255f8d8219
Steven Sarria <ssarr012@fiu.edu>
2022-06-21 14:52:44 -0400

    site update

--- src/components/BacklotTeam/teampage.js
+++ src/components/BacklotTeam/teampage.js
@@ -35,31 +35,7 @@ const TeamPage = () => {
                     </Card>
                 </div>
                 
-                <div className="col-md-6">
-                    <Card className='border-0'>
-                        <Card.Img width="100%" variant="top" src={JP} />
-                        <Card.Body className="card-body">
-                            <Card.Title className="text-center mb-2">
-                                <h3 class="mb-3">JP Matias</h3>
-                                <h6>
-                                    <i>Co-Host</i>
-                                </h6>
-                            </Card.Title>
-                            <Card.Text className="podcast-text">
-                                Producer, podcast host, #1 fan. JP joined the Backlot team earlier this year, after being a frequent guest on the show. Rounding out the Backlot trio, his knowledge of music and film caps off the team, although his ability to reel in his other two co-hosts is quite possibly the best thing for our listeners. 
-                            </Card.Text>
-                            <div className='text-center mt-3'>
-                                <a href="https://twitter.com/sarria_steven?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png" className="social-icons-team"></img></a>
-                                <a href="https://www.instagram.com/_paulymath/?hl=en"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" className="social-icons-insta"></img></a>
-                                <a href="https://www.youtube.com/channel/UCjzTSGj_1r7K-W3kzZGYIdw"><img src="https://i.pinimg.com/originals/de/1c/91/de1c91788be0d791135736995109272a.png" className="social-icons-team"></img></a>
-                            </div>
-                        </Card.Body>
-                        {/* <Card.Footer className='p-5 m-5'>
-                            <h6 className="text-center"><a href='//linktr.ee/insidethebacklot/'>Listen Now</a></h6>
-                        </Card.Footer> */}
-                    </Card>
-                </div>
-
+                
                 <div className="col-md-6">
                     <Card className='border-0'>
                         <Card.Img width="100%" variant="top" src={John} />
--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -13,11 +13,12 @@ const HomePage = () => {
             <div className="">
                 <div className="jumbotron home-jumbo mt-5 pt-5 d-flex flex-wrap bg-medium">
                     <div className="col-12 col-lg-6">
-                        <img className="featured-jumbo" src="https://variety.com/wp-content/uploads/2020/04/Invincible.jpg?w=1000" />
+                        <img className="featured-jumbo" src="https://image.cnbcfm.com/api/v1/image/107077136-1655401835436-lightyear_Cropped.jpg?v=1655401936&w=1600&h=900" />
                     </div>
                     <div className="col-12 col-lg-6" >
-                        <h1 className="display-5 pt-5 text-black">Loki: Mid-Season Review</h1>
-                        <p className="lead text-black">Steven reviews the first half of Marvel's newest series, <i>Loki.</i><br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000521517803'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/16x53tGDRgC5wJGUnMKvif?si=c126d3c982e4484b'>Listen on Spotify</a></p>
+                        <h1 className="display-5 pt-5 text-black">Lightyear, Jurassic World Dominion & Top Gun: Maverick</h1>
+                        <p className="lead text-black">Steven and John are back to discuss this trio of recently released blockbusters.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000567288804'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/4qNO5RORWgr2bcOGPOFSnn?si=s-lgYz4VTSiCiNy_Biu5rw
+https://open.spotify.com/episode/4qNO5RORWgr2bcOGPOFSnn?si=s-lgYz4VTSiCiNy_Biu5rw'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -29,6 +30,19 @@ const HomePage = () => {
             <h3 className="text-center sections">PODCASTS</h3>
 
             <CardDeck className="podcasts pb-5">
+                <Card className='podcast-card'>
+                    <Card.Img variant="top" src="https://www.hollywoodreporter.com/wp-content/uploads/2022/04/LIGHTYEAR-ONLINE-USE-b625_15k_pub.pub16.474-H-2022.jpg?w=681&h=383&crop=1" />
+                    <Card.Body className="podcast-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>Lightyear, Jurassic World Dominion and Top Gun: Mavericl</Card.Title>
+                        <Card.Text className="podcast-text">
+                            Steven and John are back to discuss this trio of recently released films.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='https://open.spotify.com/episode/4qNO5RORWgr2bcOGPOFSnn?si=s-lgYz4VTSiCiNy_Biu5rw'>Listen On Spotify</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000567288804'>Listen On Apple Podcasts</a></h6>
+                    </Card.Footer>
+                </Card>
                 <Card className='podcast-card'>
                     <Card.Img variant="top" src="https://www.denofgeek.com/wp-content/uploads/2021/03/Invincible-Amazon-Review.jpg?fit=1920%2C1080" />
                     <Card.Body className="podcast-body">
@@ -43,29 +57,16 @@ const HomePage = () => {
                     </Card.Footer>
                 </Card>
                 <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://miro.medium.com/max/3840/0*CnpPQBdzDHEYcsD2.jpeg" />
+                    <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BOWEwNGJiZWYtNTc3NC00N2QyLWIzOTQtM2YwOTY2N2NmYWQ2XkEyXkFqcGdeQXNuZXNodQ@@._V1_.jpg" />
                     <Card.Body className="podcast-body">
                         <Card.Title style={{ fontSize: "2rem" }}>Mortal Kombat</Card.Title>
                         <Card.Text className="podcast-text">
-                            Steven, JP & John discuss this year's indie gem, <i>Mortal Kombat.</i>
+                            Steven, JP & John discuss 2021's indie gem, <i>Mortal Kombat.</i>
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/5VvfYv5Bzm6BveQcMMz0cJ?si=a55e060313984fef'>Listen On Spotify</a></h6>
                         <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000519515819'>Listen On Apple Podcasts</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://cdn.onebauer.media/one/media/605b/69a9/dbf2/7f24/48f5/9560/godzilla-vs-kong.jpg?format=jpg&quality=80&width=850&ratio=16-9&resize=aspectfill" />
-                    <Card.Body className="podcast-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>93rd Annual Academy Awards Recap</Card.Title>
-                        <Card.Text className="podcast-text">
-                            Steven and John discuss the odd choices made during this year's Oscars ceremony.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='https://open.spotify.com/episode/6ya8oqcI7suFTGCtmpugSc?si=70d3e7e3f9d44299'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=10005188749573'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
             </CardDeck>

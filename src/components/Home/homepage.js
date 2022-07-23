835c9d9e708e4e2a9136f730400edf3ac2b91ef1
Steven Sarria <ssarr012@fiu.edu>
2022-07-23 17:04:36 -0400

    updated homepage

--- src/components/Home/homepage.js
+++ src/components/Home/homepage.js
@@ -13,11 +13,11 @@ const HomePage = () => {
             <div className="">
                 <div className="jumbotron home-jumbo mt-5 pt-5 d-flex flex-wrap bg-medium">
                     <div className="col-12 col-lg-6">
-                        <img className="featured-jumbo" src="https://starwarsblog.starwars.com/wp-content/uploads/2022/05/obi-wan-kenobi-is-here_TALL-298.jpg" />
+                        <img className="featured-jumbo" src="https://www.shortlist.com/media/imager/202206/56613-original.jpg" />
                     </div>
                     <div className="col-12 col-lg-6" >
-                        <h1 className="display-5 pt-5 text-black">Obi-Wan Kenobi and Elvis</h1>
-                        <p className="lead text-black">Steven and John discuss Disney's <i>Obi-Wan Kenobi</i> and Baz Luhrmann's <i>Elvis</i>.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000568028724'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/3U1vWaWjuDdkfijG71bAoe?si=0zHxrXIjSQeXb2X77-QDyw&context=spotify%3Ashow%3A4qRzBpNjTMGSKWTixYZQtY'>Listen on Spotify</a></p>
+                        <h1 className="display-5 pt-5 text-black">Thor: Love and Thunder</h1>
+                        <p className="lead text-black">Steven and John Marvel's latest film, <i>Thor: Love and Thunder</i>.<br /><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000570744034'>Listen on Apple Podcasts</a><br /><a href='https://open.spotify.com/episode/3ocnoSdNQEUjEy4CV3u6Wc?si=16c56369cfc84941'>Listen on Spotify</a></p>
                     </div>
                 </div>
             </div>
@@ -29,6 +29,19 @@ const HomePage = () => {
             <h3 className="text-center sections">PODCASTS</h3>
 
             <CardDeck className="podcasts pb-5">
+                <Card className='podcast-card'>
+                    <Card.Img variant="top" src="https://www.cnet.com/a/img/resize/4dd4458d590efe4d555dad15f026691dd2eb313f/2022/05/26/e3d5b150-9198-47cd-9716-82fae2f5037d/thor-love-and-thunder-6.jpg?auto=webp&fit=crop&height=675&width=1200" />
+                    <Card.Body className="podcast-body">
+                        <Card.Title style={{ fontSize: "2rem" }}>Thor: Love and Thunder</Card.Title>
+                        <Card.Text className="podcast-text">
+                            Steven and John discuss the MCU's latest entry: <i>Thor: Love and Thunder.</i>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <h6 className="text-center"><a href='https://open.spotify.com/episode/3ocnoSdNQEUjEy4CV3u6Wc?si=16c56369cfc84941'>Listen On Spotify</a></h6>
+                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000570744034'>Listen On Apple Podcasts</a></h6>
+                    </Card.Footer>
+                </Card>
                 <Card className='podcast-card'>
                     <Card.Img variant="top" src="https://consequence.net/wp-content/uploads/2022/06/elvis-review-austin-butler-2.jpg?quality=80&w=1031&h=580&crop=1&resize=1031%2C580&strip" />
                     <Card.Body className="podcast-body">
@@ -53,19 +66,6 @@ const HomePage = () => {
                     <Card.Footer>
                         <h6 className="text-center"><a href='https://open.spotify.com/episode/4qNO5RORWgr2bcOGPOFSnn?si=s-lgYz4VTSiCiNy_Biu5rw'>Listen On Spotify</a></h6>
                         <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000567288804'>Listen On Apple Podcasts</a></h6>
-                    </Card.Footer>
-                </Card>
-                <Card className='podcast-card'>
-                    <Card.Img variant="top" src="https://www.denofgeek.com/wp-content/uploads/2021/03/Invincible-Amazon-Review.jpg?fit=1920%2C1080" />
-                    <Card.Body className="podcast-body">
-                        <Card.Title style={{ fontSize: "2rem" }}>We Need To Talk About Invincible</Card.Title>
-                        <Card.Text className="podcast-text">
-                            Steven, JP & John discuss Amazon Prime's <i>Invincble</i>.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <h6 className="text-center"><a href='https://open.spotify.com/episode/16x53tGDRgC5wJGUnMKvif?si=d574f37e712243b4'>Listen On Spotify</a></h6>
-                        <h6 className="text-center"><a href='https://podcasts.apple.com/us/podcast/inside-the-backlot/id1460214563?i=1000521517803'>Listen On Apple Podcasts</a></h6>
                     </Card.Footer>
                 </Card>
             </CardDeck>
